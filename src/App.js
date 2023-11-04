76fc0a4d7291e48859b2c9940c54837761353b29
Peter Wang <69640977+pwang1092@users.noreply.github.com>
2023-11-04 13:24:46 -0700

    Home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./Components/Navbar.js"; 
+import Home from "./Components/Home.js"; 
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Home></Home>
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Home.css';
+import CAD from '../Images/CAD.png';
+import {Link} from 'react-scroll'; 
+
+const intro = () => {
+    return (
+        <section id="intro">
+            <div className="introContent">
+                <span className="hello">Welcome to</span>
+                <span className="introText">
+                    <span className="introName">'SC ARC <br/></span>
+                    the Advanced Robotic Combat Design Team of USC
+                </span>
+                <p className="introPara"><br/>Educating students in various fields of engineering and business through combat robotics.</p>
+                <Link>
+                    <button className="btn">
+                        About Us
+                    </button>
+                </Link>
+            </div>
+            <img src={CAD} alt="CAD" className="bg"/>
+        </section>
+    )
+}
+
+export default intro
